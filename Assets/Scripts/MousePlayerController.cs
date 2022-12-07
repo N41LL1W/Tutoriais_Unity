137570693838aaeee52e3c70132cd48cf5882361
Willian Gomes de Sá Batista <52001533+N41LL1W@users.noreply.github.com>
2022-12-07 00:53:23 -0300

    MoveCaracterByMouse v0.1

---
+++ Assets/Scripts/MousePlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class MousePlayerController : MonoBehaviour
+{
+    public LayerMask movementMask;
+    private Camera cam;
+    private PlayerMotor motor;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                motor.MoveToPoint(hit.point);
+            }
+        }
+    }
+}
